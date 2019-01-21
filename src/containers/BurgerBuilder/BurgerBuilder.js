5c4b1455c47eed754709cfc3f0eee4c7a11177d6
y.matin <yuri.matin@artezio.com>
2019-01-21 17:12:56 +0300

    lesson 110 started

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/AuxReact';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal'
 
 const INGREDIENTS_PRICES = {
     salad: 0.3,
