906a6e96b290104a1f131d80b355c6a8dd371237
ymatin <spacemarineharon@mail.ru>
2019-01-23 21:23:20 +0300

    lecture 138 done, sideDrawer component was added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/AuxReact'
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <div className={classes.Content}>
             {props.children}
         </div>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
+        <Logo heiht="80%"/>
         <nav>
             <NavigationItems/>
         </nav>
