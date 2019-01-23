b3922ede97a53e9968b372cdb1ce9d757affd487
ymatin <spacemarineharon@mail.ru>
2019-01-23 21:58:45 +0300

    lecture 139 done

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,10 +3,12 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 
-const sideDrawer = (props) => {
+const sideDrawer = () => {
     return (
       <div className={classes.SideDrawer}>
-          <Logo height="11%"/>
+       <div className={classes.Logo}>
+          <Logo />
+       </div>
           <nav>
               <NavigationItems />
           </nav>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,10 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo heiht="80%"/>
-        <nav>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
