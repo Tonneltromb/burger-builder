df3b975f41be5be05ca9de726d99e9d3f48b1b55
ymatin <MatinYA@lad24.ru>
2019-04-02 12:45:36 +0300

    new branch with redux saga

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import {BrowserRouter} from "react-router-dom";
 import {createStore, applyMiddleware, compose, combineReducers} from "redux";
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
 import App from './App';
@@ -11,18 +12,21 @@ import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import {watchAuth} from './store/sagas'
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
     auth: authReducer
 });
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : null || compose;
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, sagaMiddleware)
 ));
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
